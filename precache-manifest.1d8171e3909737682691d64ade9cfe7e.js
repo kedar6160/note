668ebd31e09197256ac517134007c70890599d75
Kedar <kedardixit6160@gmail.com>
2023-04-08 01:04:14 +0530

    Updates

---
+++ precache-manifest.1d8171e3909737682691d64ade9cfe7e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "62839f00b221011f1414",
+    "url": "/note/static/css/main.7387c294.chunk.css"
+  },
+  {
+    "revision": "62839f00b221011f1414",
+    "url": "/note/static/js/main.62839f00.chunk.js"
+  },
+  {
+    "revision": "7fc5741dfb5d402e95fa",
+    "url": "/note/static/js/1.7fc5741d.chunk.js"
+  },
+  {
+    "revision": "de1511c34481e4f1b82c",
+    "url": "/note/static/js/2.de1511c3.chunk.js"
+  },
+  {
+    "revision": "e7f4576d6381548a05d5",
+    "url": "/note/static/js/runtime~main.e7f4576d.js"
+  },
+  {
+    "revision": "0deaff501d784d29174000e83293767d",
+    "url": "/note/index.html"
+  }
+];
